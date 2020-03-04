882cba129d0230e7b6c61d31de6112a84bbd8adf
Alexei Matveev <alexei.matveev@gmail.com>
2020-03-04 14:07:06 +0100

    AM: example usage

--- project.clj
+++ project.clj
@@ -3,6 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 ;; https://github.com/clojure/java.jdbc
+                 [org.clojure/java.jdbc "0.7.11"]
+                 ;; https://github.com/h2database/h2database --- JAR
+                 ;; file size is 2.2M
+                 [com.h2database/h2 "1.4.200"]]
   :main hello-h2.core
   :repl-options {:init-ns hello-h2.core})
--- src/hello_h2/core.clj
+++ src/hello_h2/core.clj
@@ -1,4 +1,27 @@
-(ns hello-h2.core)
+;;
+;; Clojure + H2 example at [1] uses outdated version of
+;; clojure.java.jdbc.
+;;
+;; [1] http://makble.com/using-h2-in-memory-database-in-clojure
+;;
+(ns hello-h2.core
+  (:require [clojure.java.jdbc :as j]))
+
+(def db
+  {:classname "org.h2.Driver"
+   :subprotocol "h2:mem"
+   :subname "demo;DB_CLOSE_DELAY=-1"
+   :user "sa"
+   :password ""})
 
 (defn -main []
-  (println "Hello, World!"))
+  (j/db-do-commands db
+                    (j/create-table-ddl :filetable
+                                        [[:name "varchar(3200)"]
+                                         [:path "varchar(3200)"]
+                                         [:origname "varchar(3200)"]]))
+  (j/insert! db :filetable {:name "file-name"
+                            :path "file/path"
+                            :origname "original-name"})
+  (println
+   (j/query db ["select * from filetable"])))
