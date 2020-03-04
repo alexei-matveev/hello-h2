10eca89556c344412a2ae95f289a20ce5801e597
Alexei Matveev <alexei.matveev@gmail.com>
2020-03-04 20:09:01 +0100

    AM: nested call

--- src/hello_h2/core.clj
+++ src/hello_h2/core.clj
@@ -16,38 +16,41 @@
 ;; For a million rows it takes about 10s  + n * 1s to execute. Of that
 ;; 10s is  spent populating  the table  and about 1s  for each  of the
 ;; aggregate funcitons.
+(defn task [db]
+  (let [ddl (jdbc/create-table-ddl :kvtable
+                                   [[:key "varchar(256)"]
+                                    [:value :double]])]
+    (jdbc/db-do-commands db ddl))
+  (try
+    (do
+      (time
+       (jdbc/insert-multi! db
+                           :kvtable
+                           [:key :value]
+                           (for [x (range (* 1000 1000))]
+                             [(rand-nth ["a" "b"]) (rand 10)])))
+      (println
+       (jdbc/query db ["select KeY as k, VaLuE as V from kvtable limit 5"]))
+
+      ;; Numeric   literals  "0.0"   and   "0.0e0"  in   H2  SQL   are
+      ;; decimals. The  expression "i  + 0.0"  with integer  "i" would
+      ;; also be a  decimal.  You probably want  doubles.  Use "cast(i
+      ;; as double)"  to aggregate.  Aggregate functions  median() and
+      ;; percentile_cont() apparently always return a decimal.
+      (println
+       (time
+        (jdbc/query db
+                    [(str "select key"
+                          ", avg(value) as avg"
+                          ", median(value) as p50"
+                          ", percentile_cont(0.95) within group (order by value) as p95"
+                          "  from kvtable group by key")]))))
+    ;; Without try & finally, if any  of the above fails, the table is
+    ;; not deleted.  In Cider you would need to M-x sesmon-restart:
+    (finally
+      (let [ddl (jdbc/drop-table-ddl :kvtable)]
+        (jdbc/db-do-commands db ddl)))))
+
 (defn -main []
   (jdbc/with-db-connection [db db]
-    (jdbc/db-do-commands db
-                         (jdbc/create-table-ddl :kvtable
-                                                [[:key "varchar(256)"]
-                                                 [:value :double]]))
-    (try
-      (do
-        (time
-         (jdbc/insert-multi! db
-                             :kvtable
-                             [:key :value]
-                             (for [x (range (* 1000 1000))]
-                               [(rand-nth ["a" "b"]) (rand 10)])))
-        (println
-         (jdbc/query db ["select KeY as k, VaLuE as V from kvtable limit 5"]))
-
-        ;; Numeric literals "0.0" and "0.0e0"  in H2 SQL are decimals. The
-        ;; expression "i + 0.0" with integer  "i" would also be a decimal.
-        ;; You  probably  want  doubles.    Use  "cast(i  as  double)"  to
-        ;; aggregate.  Aggregate functions  median() and percentile_cont()
-        ;; apparently always return a decimal.
-        (println
-         (time
-          (jdbc/query db
-                      [(str "select key"
-                            ", avg(value) as avg"
-                            ", median(value) as p50"
-                            ", percentile_cont(0.95) within group (order by value) as p95"
-                            "  from kvtable group by key")]))))
-      ;; Without try &  finally, if any of the above  fails, the table
-      ;; is   not  deleted.    In  Cider   you  would   need  to   M-x
-      ;; sesmon-restart:
-      (finally
-        (jdbc/db-do-commands db (jdbc/drop-table-ddl :kvtable))))))
+    (task db)))
