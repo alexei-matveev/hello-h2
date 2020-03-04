ed1c1d4a3014b061ec5ab8f3ecfd2746bf9ff703
Alexei Matveev <alexei.matveev@gmail.com>
2020-03-04 18:18:58 +0100

    AM: percentile_cont() works

--- src/hello_h2/core.clj
+++ src/hello_h2/core.clj
@@ -13,8 +13,9 @@
 ;; FWIW, it is not "sa"/"".
 (def db {:dbtype "h2:mem", :dbname "demo"})
 
-;; For a million  rows it takes about  12s to execute. Of  that 10s is
-;; spent populating the table.
+;; For a million rows it takes about 10s  + n * 1s to execute. Of that
+;; 10s is  spent populating  the table  and about 1s  for each  of the
+;; aggregate funcitons.
 (defn -main []
   (jdbc/with-db-connection [db db]
     (jdbc/db-do-commands db
@@ -31,14 +32,16 @@
      (jdbc/query db ["select KeY as k, VaLuE as V from kvtable limit 5"]))
 
     ;; Numeric literals "0.0" and "0.0e0"  in H2 SQL are decimals. The
-    ;; expression  "integer  +  0.0"  would also  be  a  decimal.  You
+    ;; expression  "integer  + 0.0"  would  also  be a  decimal.   You
     ;; probably want  doubles. Use  "cast(value as double)"  for that.
-    ;; Median is apparently always a decimal.
+    ;; Aggregate funcitons  median() and  percentile_cont() apparently
+    ;; always return a decimal.
     (println
      (time
       (jdbc/query db
                   [(str "select key"
                         ", avg(value) as avg"
-                        ", median(value) as median"
+                        ", median(value) as p50"
+                        ", percentile_cont(0.95) within group (order by value) as p95"
                         "  from kvtable group by key")])))
     (jdbc/db-do-commands db (jdbc/drop-table-ddl :kvtable))))
