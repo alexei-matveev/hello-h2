cdeb7f59efb75b70a529216fa094f3d66c27c9fe
Alexei Matveev <alexei.matveev@gmail.com>
2020-03-05 20:01:41 +0100

    AM: comments

--- src/hello_h2/fasta.clj
+++ src/hello_h2/fasta.clj
@@ -1,16 +1,22 @@
 (ns hello-h2.fasta)
 
 ;;
-;; See e.g.GenBank dor SARS-CoV-2 [1].
+;; This is a  hot topic at the beginning of  2020. See e.g.GenBank dor
+;; SARS-CoV-2 [1].
 ;;
 ;; [1] https://www.ncbi.nlm.nih.gov/genome/genomes/86693
 ;;
-;; Curl can list FTP dirs too, just add a trailing slash:
+;; TIL, curl can list FTP dirs too, just add a trailing slash!
 ;;
-;;    $ curl ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/009/858/895/GCA_009858895.3_ASM985889v3/
-;;    $ curl -LO ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/009/858/895/GCA_009858895.3_ASM985889v3/GCA_009858895.3_ASM985889v3_genomic.fna.gz
-;;    $ zless GCA_009858895.3_ASM985889v3_genomic.fna.gz | md5sum
-;;    cacc9084842139321adc1f83326d6819  -
+;;     $ curl -LO ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/009/858/895/GCA_009858895.3_ASM985889v3/GCA_009858895.3_ASM985889v3_genomic.fna.gz
+;;
+;;     $ zless GCA_009858895.3_ASM985889v3_genomic.fna.gz | md5sum
+;;     cacc9084842139321adc1f83326d6819  -
+;;
+;;     $ zless resources/GCA_009858895.3_ASM985889v3_genomic.fna.gz | head  -3
+;;     >MN908947.3 Severe acute respiratory syndrome coronavirus 2 isolate Wuhan-Hu-1, complete genome
+;;     ATTAAAGGTTTATACCTTCCCAGGTAACAAACCAACCAACTTTCGATCTCTTGTAGATCTGTTCTCTAAACGAACTTTAA
+;;     AATCTGTGTGGCTGTCACTCGGCTGCATGCTTAGTGCACTCACGCAGTATAATTAATAACTAATTACTGTCGTTGACAGG
 ;;
 
 ;; MN908947.3 Severe acute respiratory syndrome coronavirus 2 isolate
@@ -26,7 +32,12 @@
                   (clojure.java.io/input-stream url))]
     (slurp in)))
 
-;; FIXME: very fragile. Always assumes header line:
+;;
+;; FIXME: very fragile. Always assumes a header line. See FASTA Format
+;; [2].
+;;
+;; [2] https://en.wikipedia.org/wiki/FASTA_format
+;;
 (defn parse-fasta [text]
   (let [lines (clojure.string/split-lines text)
         header (first lines)
@@ -34,8 +45,9 @@
         sequence (apply str (rest lines))]
     {:description description, :sequence sequence}))
 
-;; SARS-CoV-2 ist about 30Kb: (count (slurp-gz url)) = 30373. Of which
-;; only (count (:sequence (parse-fasta faste)) = 29903.
+;; SARS-CoV-2 genom is exactly 29903 long [1].  (count (slurp-gz url))
+;; =  30373. Of  which only  (count (:sequence  (parse-fasta text))  =
+;; 29903.
 (defn- main []
   (let [fasta (slurp-gz url)]
     (parse-fasta fasta)))
