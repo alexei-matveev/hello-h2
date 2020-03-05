aa63c55c72f45e2d9ada6c7bc7b262b4f509cb2d
Alexei Matveev <alexei.matveev@gmail.com>
2020-03-05 19:35:15 +0100

    AM: poor man fasta parser

--- src/hello_h2/fasta.clj
+++ src/hello_h2/fasta.clj
@@ -1,6 +1,10 @@
 (ns hello-h2.fasta)
 
 ;;
+;; See e.g.GenBank dor SARS-CoV-2 [1].
+;;
+;; [1] https://www.ncbi.nlm.nih.gov/genome/genomes/86693
+;;
 ;; Curl can list FTP dirs too, just add a trailing slash:
 ;;
 ;;    $ curl ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/009/858/895/GCA_009858895.3_ASM985889v3/
@@ -15,14 +19,23 @@
               "/genomes/all/GCA/009/858/895/GCA_009858895.3_ASM985889v3"
               "/GCA_009858895.3_ASM985889v3_genomic.fna.gz"))
 
-;; Clojure  "slurp" will  happily  download the  binars  too, but  the
-;; String would be garbage:
+;; Clojure "slurp" will happily download  and slurp a binary file too,
+;; but the String would be garbage. We need this:
 (defn- slurp-gz [url]
   (with-open [in (java.util.zip.GZIPInputStream.
                   (clojure.java.io/input-stream url))]
     (slurp in)))
 
+;; FIXME: very fragile. Always assumes header line:
+(defn parse-fasta [text]
+  (let [lines (clojure.string/split-lines text)
+        header (first lines)
+        description (subs header 1)
+        sequence (apply str (rest lines))]
+    {:description description, :sequence sequence}))
+
+;; SARS-CoV-2 ist about 30Kb: (count (slurp-gz url)) = 30373. Of which
+;; only (count (:sequence (parse-fasta faste)) = 29903.
 (defn- main []
   (let [fasta (slurp-gz url)]
-    ;; SARS-CoV-2 ist about 30Kb. Here Count = 30373:
-    (count (fasta))))
+    (parse-fasta fasta)))
