1e812c77ebdc0718a3a7bbb1ef3ef48c7cfaa669
Alexei Matveev <alexei.matveev@gmail.com>
2020-03-13 21:40:23 +0100

    AM: csvread() from classpath

--- resources/schema.sql
+++ resources/schema.sql
@@ -1,5 +1,7 @@
-drop table xyz if exists;
-create table xyz (x integer, y integer, z integer);
+-- CSVREAD() is an H2 Feature and can also read from the classpath:
+drop table meta if exists;
+create table meta (x integer, y integer, z integer)
+as select * from csvread('classpath:meta.csv');
 
 drop table zyx if exists;
 create table zyx (x integer, y integer, z integer);
